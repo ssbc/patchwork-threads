c9f08f2d4dda75b0327df7f8f35e86497d545d7a
Paul Frazee <pfrazee@gmail.com>
2015-11-28 00:36:10 -0600

    test: missing parents are noted

--- test/flatten.js
+++ test/flatten.js
@@ -109,6 +109,56 @@ tape('flattenThread correctly weaves mentions into the thread', function (t) {
   })
 })
 
+tape('flattenThread correctly detects missing parents', function (t) {
+
+  var db = sublevel(level('test-patchwork-threads-flatten-missing-parents', {
+    valueEncoding: defaults.codec
+  }))
+  var ssb = SSB(db, defaults)
+
+  var alice = ssb.createFeed(ssbKeys.generate())
+  var bob = ssb.createFeed(ssbKeys.generate())
+  var carla = ssb.createFeed(ssbKeys.generate())
+
+  // load test thread into ssb
+  alice.add({ type: 'post', text: 'a' }, function (err, msgA) {
+    if (err) throw err
+
+    // first reply
+    bob.add({ type: 'post', text: 'b', root: msgA.key, branch: msgA.key }, function (err, msgB) {
+      if (err) throw err
+
+      // second reply
+      carla.add({ type: 'post', text: 'c', root: msgA.key, branch: msgB.key }, function (err, msgC) {
+        if (err) throw err
+
+        // fetch thread
+        threadlib.getPostThread(ssb, msgA.key, {}, function (err, thread) {
+          if (err) throw err
+
+          // delete the first reply
+          // 0 & 1 are msgB (0 for root and 1 for branch) delete them both
+          thread.related.splice(0, 2)
+          console.log(thread.related[0].key)
+
+          // now flatten
+          var msgs = threadlib.flattenThread(thread)
+          console.log(msgs[0].key)
+          t.equal(msgs.length, 3)
+          // ensure msgs were interpretted correctly
+          t.equal(msgs[0].key, msgA.key)
+          t.equal(!!msgs[0].isNotFound, false)
+          t.equal(msgs[1].key, msgB.key) 
+          t.equal(!!msgs[1].isNotFound, true) // our missing post
+          t.equal(msgs[2].key, msgC.key)
+          t.equal(!!msgs[2].isNotFound, false)
+          t.end()
+        })
+      })
+    })
+  })
+})
+
 function customTimeCreateMsg (keys, timestamp, content) {
   return ssbKeys.signObj(keys, {
     previous: null,
